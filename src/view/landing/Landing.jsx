4dd9abb963f42e134e98d440e6e0f562e4142187
JONAAVILA <jona_1969@hotmail.com>
2024-10-17 18:49:08 -0300

    first commit

---
+++ src/view/landing/Landing.jsx
@@ -0,0 +1,20 @@
+import './landing.css';
+
+const Landing = ()=>{
+    return(
+        <section className='landing_section' >
+            <h1>
+                <div className='box_heading_harmony' >
+                    <span>ARMO</span>
+                    <span className='nia' >NIA</span>
+                </div>
+                <div className='box_heading_natural' >
+                    <span>NATU</span>
+                    <span className='ral' >RAL</span>
+                </div>
+            </h1>
+        </section>
+    )
+}
+
+export default Landing
