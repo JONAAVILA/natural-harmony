14d38d6b6727a57d2068efc1e0d25395b3d4fa00
JONAAVILA <jona_1969@hotmail.com>
2024-10-19 12:29:15 -0300

    login created

--- src/view/login/Login.jsx
+++ src/view/login/Login.jsx
@@ -1,10 +1,19 @@
+import ButtonCircle from '../../components/button/buttonCircle/ButtonCircle';
 import LoginForm from '../../components/forms/login/Login.form';
 import './login.css';
 
 const Login = ()=>{
     return(
-        <section>
-            <LoginForm/>
+        <section className='section_login' >
+            <h1 className='heading_login' >
+                <span>INGRE</span>
+                <span className='sar' >SAR</span>
+            </h1>
+            <div>
+                <h2>QUE BUENO VERTE</h2>
+                <h3>VAMOS A PEDIRTE UNOS DATOS</h3>
+                <LoginForm/>
+            </div>
         </section>
     )
 }
