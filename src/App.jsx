a68d6cf66a619f9e7a88f137c34f090e01a1f67b
JONAAVILA <jona_1969@hotmail.com>
2024-10-22 11:11:56 -0300

    signin component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Landing from './view/landing/Landing'
 import Login from './view/login/Login'
+import Singin from './view/singin/Singin'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Landing/>} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/singin' element={<Singin/>} />
       </Routes>
     </BrowserRouter>
   )
--- src/view/singin/Singin.jsx
+++ src/view/singin/Singin.jsx
@@ -4,8 +4,15 @@ const Singin = ()=>{
     return(
         <section className='container_singin' >
             <h1>
-                
+                <span>REGIS</span>
+                <br/>
+                <span className='trar' >TRAR</span>
+                <br/>
+                <span className='se' >SE</span>
             </h1>
+            
         </section>
     )
 }
+
+export default Singin
