1d8119d74293a4847eb0fa19c86182e05d84a0e0
JONAAVILA <jona_1969@hotmail.com>
2024-10-26 12:50:26 -0300

    complete signin

--- src/view/signin/Signin.jsx
+++ src/view/signin/Signin.jsx
@@ -4,13 +4,16 @@ import './signin.css';
 const Signin = ()=>{
     return(
         <section className='container_signin' >
-            <h1>
-                <span>REGIS</span>
-                <br/>
-                <span className='trar' >TRAR</span>
-                <br/>
-                <span className='se' >SE</span>
-            </h1>
+            <div className='box_heading_signin' >
+                <h1>
+                    <span>REGIS</span>
+                    <br/>
+                    <span className='trar' >TRAR</span>
+                    <br/>
+                    <span className='se' >SE</span>
+                </h1>
+                <p>al crear el usuario aceptas los terminos y condiciones de Natural Armonia</p>
+            </div>
             <CreateUser/>
         </section>
     )
