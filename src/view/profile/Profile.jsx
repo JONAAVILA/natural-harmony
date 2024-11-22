851f7da9a2b342261b9824fdb04750e8f18239f8
JONAAVILA <jona_1969@hotmail.com>
2024-11-22 11:34:55 -0300

    profile styled

--- src/view/profile/Profile.jsx
+++ src/view/profile/Profile.jsx
@@ -6,6 +6,9 @@ import { useEffect } from 'react'
 const Profile = ()=>{
     const navigate = useNavigate()
     const user = useUser()
+    const startName = user.name.slice(0,user.name.length / 2).toUpperCase()
+    const endName = user.name.slice(user.name.length / 2).toUpperCase()
+    const surname = user.surname.toUpperCase()
 
     useEffect(()=>{
         if(!user) navigate('/login')
@@ -13,8 +16,18 @@ const Profile = ()=>{
 
     return(
         <section>
-            <h1>{user.name}</h1>
-            <h2>{user.surname}</h2>
+            <div className='container_heading_profile' >
+                <h1 className='heading_profile' >
+                    <span>{startName}</span>
+                    <span>{endName}</span>
+                </h1>
+                <h2>{surname}</h2>
+            </div>
+            <div className='box_info_profile' >
+                <h3>{user.email}</h3>
+                <h3>{user.location}</h3>
+                <h3>{user.country}</h3>
+            </div>
         </section>
     )
 }
