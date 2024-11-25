ef6b78a4050e14fcfe297ccea3eb7acbcc6c23a7
JONAAVILA <jona_1969@hotmail.com>
2024-11-25 12:03:19 -0300

    socialnetwork and icon instagram created

---
+++ src/components/button/socialNetworks/SocialNetworks.jsx
@@ -0,0 +1,12 @@
+import Instagram from '../../icons/instagram/Instagram'
+import './socialNetworks.css'
+
+const SocialNetworks = ()=>{
+    return(
+        <div>
+            <Instagram/>
+        </div>
+    )
+}
+
+export default SocialNetworks
