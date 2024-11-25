47e7d508ac0bc2bd246571b27df68f74943c9ff7
JONAAVILA <jona_1969@hotmail.com>
2024-11-25 11:40:27 -0300

    button change lenguage fixed

--- src/components/button/buttonLenguage/ButtonLenguage.jsx
+++ src/components/button/buttonLenguage/ButtonLenguage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import LenguageIcon from '../../icons/lenguage/Lenguage.icon'
 import './buttonLenguage.css'
-import { useEffect } from 'react'
 
 const ButtonLenguage = ({handleLenguage})=>{
     const [lenguage, setLenguage] = useState('es')
@@ -20,16 +20,15 @@ const ButtonLenguage = ({handleLenguage})=>{
         if(lenguage === 'es') setLenguage('it')
         if(lenguage === 'it') setLenguage('es')
     }
-
     
     return(
         <div className='container_lenguage_button' >
-            <button className='button_lenguage' onClick={()=>changelg()} >
+            <button className='button_lenguage' onClick={changelg} >
                 <LenguageIcon/>
             </button>
             <div className='box_lenguages' >
-                <p id='es' >español</p>
-                <p id='it' >italiano</p>
+                <p id='es' >ESPAÑOL</p>
+                <p id='it' >ITALIANO</p>
             </div>
         </div>
     )
