623dc7f011991254df46575ca1bae5ac9cc30afe
JONAAVILA <jona_1969@hotmail.com>
2024-12-03 00:00:31 -0300

    change button menu

--- src/components/button/buttonMenu/ButtonMenu.jsx
+++ src/components/button/buttonMenu/ButtonMenu.jsx
@@ -2,19 +2,11 @@ import './buttonMenu.css'
 
 const ButtonMenu = ()=>{
     return(
-        <svg
-            className='button_menu_icon'
-            width="24"  
-            height="24"  
-            viewBox="0 0 24 24"  
-            fill="none"  
-            stroke="currentColor"  
-        >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path className='path1' d="M4 6l16 0" />
-            <path className='path2' d="M4 12l16 0" />
-            <path className='path3' d="M4 18l16 0" />
-        </svg>
+       <div className='box_menu' >
+           <span className='dot top' />
+           <span className='dot mid' />
+           <span className='dot end ' />
+       </div>
     )
 }
 
