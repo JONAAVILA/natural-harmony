1022f38d898a109b30bb9a546b66c324fc250ed1
JONAAVILA <jona_1969@hotmail.com>
2024-12-03 13:07:37 -0300

    button animation created

--- src/components/button/buttonMenu/ButtonMenu.jsx
+++ src/components/button/buttonMenu/ButtonMenu.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import './buttonMenu.css'
 
 const ButtonMenu = ()=>{
+    const [ toggle, setToggle ] = useState(false)
+
+    const handleToggle = ()=>{
+        setToggle(!toggle)
+    }
+
     return(
-       <div className='box_menu' >
-           <span className='dot top' />
-           <span className='dot mid' />
-           <span className='dot end ' />
+       <div onClick={handleToggle} className='box_menu' >
+           <span className={`dot top ${toggle}`} />
+           <span className={`dot mid${toggle}`}/>
+           <span className={`dot end${toggle} `} />
        </div>
     )
 }
